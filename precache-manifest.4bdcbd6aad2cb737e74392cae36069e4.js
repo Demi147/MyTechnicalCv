f488153c6225940e91924969750d4a71b9708557
Demi147 <productionsDemigod@gmail.com>
2020-09-27 18:27:08 +0200

    Updates

---
+++ precache-manifest.4bdcbd6aad2cb737e74392cae36069e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c450036bdc63296f9e6934be9857ad",
+    "url": "/MyTechnicalCv/index.html"
+  },
+  {
+    "revision": "a9215db0173ceb826865",
+    "url": "/MyTechnicalCv/static/css/main.3fd71047.chunk.css"
+  },
+  {
+    "revision": "96edac6b4d01bdef9e20",
+    "url": "/MyTechnicalCv/static/js/2.cb62f910.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyTechnicalCv/static/js/2.cb62f910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9215db0173ceb826865",
+    "url": "/MyTechnicalCv/static/js/main.18dfa9cd.chunk.js"
+  },
+  {
+    "revision": "6e96342efa107444f4c7",
+    "url": "/MyTechnicalCv/static/js/runtime-main.3334bf8c.js"
+  }
+]);
